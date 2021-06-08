05564e6fb89f6a3c367eac32dda580c16380477d
Alexandre Repaire-Carlier <repaire.alexandre@gmail.com>
2021-06-08 19:14:38 +0200

    ajout component form, list, filter

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+
+import Card from '../UI/Card';
+import './Expenses.css';
+import ExpenseFilter from "./ExpenseFilter";
+import ExpensesList from "./ExpensesList";
+
+const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = selectedYear =>{
+        setFilteredYear(selectedYear);
+    };
+
+    const filteredexpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+
+    return (
+        <div>
+            <Card className="expenses">
+                <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+                <ExpensesList items={filteredexpenses}/>
+            </Card>
+        </div>
+
+    );
+}
+
+export default Expenses;
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,86 @@
+import React, {useState} from 'react';
+import './ExpenseForm.css';
+
+const ExpenseForm = (props) => {
+    const [entrerTitre, setEntrerTitre] = useState("");
+    const [entrerAmount, setEntrerAmount] = useState("");
+    const [entrerDate, setEntrerDate] = useState("");
+
+    /*const [userInput, setUserInput] = useState({
+       entrerTitre:'',
+       entrerMontant:'',
+       entrerDate:''
+    });*/
+
+    const titleChangeHandler = e =>{
+        setEntrerTitre(e.target.value);
+        /*setUserInput({
+            ...userInput,
+            entrerTitre:e.target.value,
+        });
+        setUserInput((prevState)=>{
+            return {...prevState,entrerTitre: e.target.value};
+        });*/
+    };
+
+    const amountChangeHandler = e =>{
+        setEntrerAmount(e.target.value);
+        /*setUserInput({
+            ...userInput,
+            entrerMontant:e.target.value,
+        });
+        setUserInput((prevState)=>{
+            return {...prevState, entrerMontant:e.target.value};
+        })*/
+    };
+
+    const dateChangeHandler = e =>{
+        setEntrerDate(e.target.value);
+        /*setUserInput({
+            ...userInput,
+            entrerDate:e.target.value,
+        });
+        setUserInput((prevState)=>{
+            return {...prevState, entrerDate:e.target.value};
+        })/*/
+    };
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        const expenseData={
+          title: entrerTitre,
+          amount:entrerAmount,
+          date:new Date(entrerDate)
+        };
+
+        props.onSaveExpenseData(expenseData);
+        setEntrerTitre('');
+        setEntrerAmount('');
+        setEntrerDate('');
+    };
+
+    return (
+        <form onSubmit={submitHandler}>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label>Titre</label>
+                    <input type="text" value={entrerTitre} onChange={titleChangeHandler}/>
+                </div>
+                <div className="new-expense__control">
+                    <label>Montant</label>
+                    <input type="number" min="0.01" step="0.01" value={entrerAmount} onChange={amountChangeHandler}/>
+                </div>
+                <div className="new-expense__control">
+                    <label>Date</label>
+                    <input type="date" min="2019-01-01" max="2022-12-31" value={entrerDate} onChange={dateChangeHandler}/>
+                </div>
+            </div>
+            <div className="new-expense__actions">
+                <button type="submit">Ajout dépense</button>
+            </div>
+        </form>
+    );
+};
+
+export default ExpenseForm;
